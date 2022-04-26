d6630b4b679153f9d7b17b2e337678e3fe570c1d
AML <andrewmloop@gmail.com>
2022-04-26 10:59:00 -0400

    Add navbar color transition on scroll

--- components/sections/Navbar.js
+++ components/sections/Navbar.js
@@ -1,18 +1,34 @@
 import { useState } from "react";
 
+
 export default function Navbar() {
   const [active, setActive] = useState(false);
-
+  // Toggle mobile menu
   const handleToggle = () => {
     setActive(!active);
   };
 
+  const [colorChange, setColorChange] = useState(false);
+  // Change the navbar's color when scrolling past the hero section
+  const handleNavbarColor = () => {
+    if (window.scrollY >= window.innerHeight) {
+      setColorChange(true);
+    } else {
+      setColorChange(false);
+    }
+  };
+
+  if (typeof window !== "undefined") {
+    window.addEventListener("scroll", handleNavbarColor);
+  }
+
+
   return (
-    <div className="sticky top-0 w-full p-2 md:p-4 bg-black z-10">
+    <div className={`fixed top-0 w-full p-2 md:p-4 z-10 ${colorChange ? "bg-black" : "bg-transparent"} transition-colors duration-[250ms]`}>
       <nav className="flex justify-between items-center">
         {/* Logo/Name */}
-        <div className="p-2 md:p-4 text-white">
-          <a href="#hero">AML</a>
+        <div className="p-2 md:p-4">
+          <a href="#hero" className="px-1 text-xl text-gold font-bold ring-[3px] ring-gold md:text-2xl">AML_</a>
         </div>
         {/* Mobile Nav Button */}
         <button
@@ -34,9 +50,9 @@ export default function Navbar() {
           }
         </button>
         {/* Nav Links */}
-        <div className={`fixed ${active ? "" : "translate-x-[100vw] md:translate-x-0"} top-0 right-0 h-screen w-[66vw] bg-light-navy transition-transform duration-[400ms] ease-in-out md:relative md:h-auto md:w-auto md:bg-inherit md:top-auto`}>
+        <div className={`fixed ${active ? "" : "translate-x-[100vw] md:translate-x-0"} top-0 right-0 h-screen w-[66vw] bg-black transition-transform duration-[400ms] ease-in-out md:relative md:h-auto md:w-auto md:bg-inherit md:top-auto`}>
           <div className="flex flex-col h-screen w-full items-center text-center first:mt-[5rem] text-2xl text-white md:flex-row md:h-auto md:first:mt-0 md:text-left md:text-xl">
-            <NavItem text="About" dest="#about" handleToggle={handleToggle} />
+            <NavItem text="Home" dest="#hero" handleToggle={handleToggle} />
             <NavItem text="Projects" dest="#projects" handleToggle={handleToggle} />
             <NavItem text="Contact" dest="#contact" handleToggle={handleToggle} />
             <button
