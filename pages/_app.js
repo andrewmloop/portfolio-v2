d35d4cba5fd9d8520d3dc5c81b6d238328bb56f4
AML <andrewmloop@gmail.com>
2022-05-09 14:04:22 -0400

    Add page meta tags

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,20 @@
 import "../styles/globals.css";
+import Head from "next/head";
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>AML</title>
+        <meta name="description" content="Andrew M Loop | Web Developer" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <meta name="msapplication-TileColor" content="#00aba9" />
+        <meta name="theme-color" content="#ffffff" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
